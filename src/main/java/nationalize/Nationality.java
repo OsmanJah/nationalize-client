856d29509aa396b56271c7eaf5960b93a614a570
Osman Jah <osmann5260@gmail.com>
2025-03-04 23:52:50 +0100

    build: added imports and toString in both classes

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,5 +1,6 @@
 package nationalize;
-
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
@@ -42,6 +43,10 @@ public class Nationality {
         this.countries = countries;
     }
 
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+    }
     public static class Country {
 
         private String countryId;
@@ -68,6 +73,10 @@ public class Nationality {
             this.probability = probability;
         }
 
+        @Override
+        public String toString() {
+            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+        }
     }
 
 }
